a42d0b928772f4dacf9bf3d6aaacd0913abbc09b
Vladyslav <vladislav.putintsev@gmail.com>
2025-11-08 15:31:24 +0200

    fixed routes

--- src/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { /*authenticate,*/ } from '../middleware/authenticate.js';
-import { createOrder } from '../controllers/orderController.js';
-import { validateCreateOrder } from '../validations/orderValidation.js';
-const router = express.Router();
-router.post('/', /*authenticate,*/ validateCreateOrder, createOrder);
-export default router;
