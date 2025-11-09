7cef447a22cfc09cc4c6221a3edaabbf322adfac
Artem <tem4ik20.1@gmail.com>
2025-11-09 19:52:58 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -21,7 +21,12 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(spec));
