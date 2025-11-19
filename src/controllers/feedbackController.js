cdf5420558ccd2b57d39f5cfaf045e6f2488510f
Vladyslav <vladislav.putintsev@gmail.com>
2025-11-19 14:32:11 +0200

    Update feedbackController.js

--- src/controllers/feedbackController.js
+++ src/controllers/feedbackController.js
@@ -1,32 +1,57 @@
 import { Feedback } from '../models/feedback.js';
+import Good from '../models/good.js';
+import Category from '../models/category.js';
 import createHttpError from 'http-errors';
 
-export const getFeedbacks = async (req, res) => {
-  const { page = 1, perPage = 10, goodId } = req.query;
-  const skip = (page - 1) * perPage;
+export const getFeedbacks = async (req, res, next) => {
+  try {
+    const { page = 1, perPage = 10, goodId } = req.query;
+    const skip = (page - 1) * perPage;
 
-  const filter = goodId ? { goodId } : {};
-  const feedbacksQuery = Feedback.find(filter);
+    const filter = goodId ? { goodId } : {};
+    const feedbacksQuery = Feedback.find(filter).lean();
 
-  const [totalFeedbacks, feedbacks] = await Promise.all([
-    feedbacksQuery.clone().countDocuments(),
-    feedbacksQuery.skip(skip).limit(perPage),
-  ]);
+    const [totalFeedbacks, feedbacks] = await Promise.all([
+      feedbacksQuery.clone().countDocuments(),
+      feedbacksQuery.skip(skip).limit(perPage),
+    ]);
 
-  const totalPages = Math.ceil(totalFeedbacks / perPage);
+    const fullFeedbacks = await Promise.all(
+      feedbacks.map(async (fb) => {
+        const good = await Good.findById(fb.goodId).select('category').lean();
 
-  res.status(200).json({
-    page,
-    perPage,
-    totalFeedbacks,
-    totalPages,
-    feedbacks,
-  });
-};
+        let categoryId = null;
+        let categoryName = null;
 
-export const createFeedback = async (req, res) => {
-  const feedback = await Feedback.create({
-    ...req.body,
-  });
-  res.status(201).json(feedback);
+        if (good?.category) {
+          const category = await Category.findById(good.category)
+            .select('_id name')
+            .lean();
+
+          if (category) {
+            categoryId = category._id;
+            categoryName = category.name;
+          }
+        }
+
+        return {
+          ...fb,
+          categoryId,
+          categoryName,
+        };
+      }),
+    );
+
+    const totalPages = Math.ceil(totalFeedbacks / perPage);
+
+    res.status(200).json({
+      page,
+      perPage,
+      totalFeedbacks,
+      totalPages,
+      feedbacks: fullFeedbacks,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
